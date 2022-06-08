64ac4a3a445fe606892c9a2232a626b5804dd2c1
Jasopaum <jason.paumier@gmail.com>
2022-06-08 11:23:35 +0200

    feat: add bounded sum

---
+++ inst/python/pydp_wrapper/sum_PyDP.py
@@ -0,0 +1,7 @@
+import pydp as dp
+from pydp.algorithms.laplacian import BoundedSum
+
+
+def pyDP_bounded_sum(input_data, epsilon, delta, lower_bound, upper_bound):
+  x = BoundedSum(epsilon=epsilon, delta=delta, lower_bound=lower_bound, upper_bound=upper_bound, dtype="float")
+  return x.quick_result(input_data)
