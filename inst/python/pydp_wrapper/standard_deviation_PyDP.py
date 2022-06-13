37c99b8a5638a4b1afbdefa2ca00b5a6baabcced
Jasopaum <jason.paumier@gmail.com>
2022-06-13 11:47:35 +0200

    feat: add standard deviation

---
+++ inst/python/pydp_wrapper/standard_deviation_PyDP.py
@@ -0,0 +1,7 @@
+import pydp as dp
+from pydp.algorithms.laplacian import BoundedStandardDeviation
+
+
+def pyDP_bounded_standard_deviation(input_data, epsilon, lower_bound, upper_bound):
+  x = BoundedStandardDeviation(epsilon=epsilon, lower_bound=lower_bound, upper_bound=upper_bound, dtype="float")
+  return x.quick_result(input_data)
