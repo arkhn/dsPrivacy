bc49f40939b1494d2af813b1933cb1b25f180f37
Jasopaum <jason.paumier@gmail.com>
2022-06-14 11:17:41 +0200

    doc: add doc on functions

--- R/boundedMeanDP.R
+++ R/boundedMeanDP.R
@@ -1,14 +1,12 @@
-#' Title
+#' @title Differentially private mean
+#' @description Computes a differentially private mean of a vector
 #'
-#' @param input_data
-#' @param epsilon
-#' @param lower_bound
-#' @param upper_bound
+#' @param input_data the input vector
+#' @param epsilon privacy budget
+#' @param lower_bound lower bound for input values
+#' @param upper_bound upper bound for input values
 #'
-#' @return
-#' @export
-#'
-#' @examples
+#' @return a numeric, differentially private mean of the input vector
 
 
 boundedMeanDP <- function(input_data, epsilon, lower_bound, upper_bound){
--- R/boundedStandardDeviationDP.R
+++ R/boundedStandardDeviationDP.R
@@ -1,14 +1,12 @@
-#' Title
+#' @title Differentially private standard deviation
+#' @description Computes a differentially private standard deviation of a vector
 #'
-#' @param input_data
-#' @param epsilon
-#' @param lower_bound
-#' @param upper_bound
+#' @param input_data the input vector
+#' @param epsilon privacy budget
+#' @param lower_bound lower bound for input values
+#' @param upper_bound upper bound for input values
 #'
-#' @return
-#' @export
-#'
-#' @examples
+#' @return a numeric, differentially private standard deviation of the input vector
 
 
 boundedStandardDeviationDP <- function(input_data, epsilon, lower_bound, upper_bound){
