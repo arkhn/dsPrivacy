a195c7ffdf3411ff229c6b2b030e75d1d6f25f55
Jasopaum <jason.paumier@gmail.com>
2022-06-14 11:17:09 +0200

    fix: remove delta argument in bounded sum

--- R/boundedSumDP.R
+++ R/boundedSumDP.R
@@ -1,18 +1,15 @@
-#' Title
+#' @title Differentially private sum
+#' @description Computes a differentially private sum of a vector
 #'
-#' @param input_data
-#' @param epsilon
-#' @param delta
-#' @param lower_bound
-#' @param upper_bound
+#' @param input_data the input vector
+#' @param epsilon privacy budget
+#' @param lower_bound lower bound for input values
+#' @param upper_bound upper bound for input values
 #'
-#' @return
-#' @export
-#'
-#' @examples
+#' @return a numeric, differentially private sum of the input vector
 
 
-boundedSumDP <- function(input_data, epsilon, delta, lower_bound, upper_bound){
-  res <- py_module$sum_PyDP$pyDP_bounded_sum(input_data, epsilon, delta, lower_bound, upper_bound)
+boundedSumDP <- function(input_data, epsilon, lower_bound, upper_bound){
+  res <- py_module$sum_PyDP$pyDP_bounded_sum(input_data, epsilon, lower_bound, upper_bound)
   return(res)
 }
