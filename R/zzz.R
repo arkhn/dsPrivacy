85e0bb438a8100541ca11ce376ad87b1873d689b
Jasopaum <jason.paumier@gmail.com>
2022-06-15 17:34:28 +0200

    t

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,6 @@ py_module <- NULL
 .onLoad <- function(libname, pkgname) {
   library(reticulate)
 
-  reticulate::use_virtualenv(virtualenv = ".venv", required = TRUE)
+  reticulate::use_virtualenv(virtualenv = "/var/lib/rock/.virtualenvs/.venv", required = TRUE)
   py_module <<- reticulate::import_from_path(module = "pydp_wrapper", path = system.file("python", package = packageName()))
 }
