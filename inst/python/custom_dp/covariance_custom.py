c91809cea5a4c40558e456668c60a475b77bcf77
Théo Ryffel <theo.leffyr@gmail.com>
2022-06-17 14:19:39 +0200

    style: apply black

--- inst/python/custom_dp/covariance_custom.py
+++ inst/python/custom_dp/covariance_custom.py
@@ -2,53 +2,45 @@ import numpy as np
 
 
 def lap(scale):
-  return np.random.laplace(0, scale)
+    return np.random.laplace(0, scale)
+
 
 def custom_bounded_covariance(x, y, epsilon, x_min, x_max, y_min, y_max):
-  """
+    """
+    Manual implementation of the covariance with differential privacy
+    """
+    # TODO distribute epsilon
+    assert len(x) == len(y), "Variables should have the same length"
+    assert x_min <= x_max, "Verify that you bounds are corrected ordered"
+    assert y_min <= y_max, "Verify that you bounds are corrected ordered"
 
-  Args:
-    x:
-    y:
-    epsilon:
-    x_min:
-    x_max:
-    y_min:
-    y_max:
+    # Crop the data using the bounds provided
+    x = np.min(x_max, np.max(x_min, x))
+    y = np.min(y_max, np.max(y_min, y))
 
-  Returns:
+    # Compute the DP count once
+    N = len(x) + np.random.laplace(0, 2 / epsilon)
 
-  """
-  # TODO distribute epsilon
-  assert len(x) == len(y), "Variables should have the same length"
-  assert x_min <= x_max, "Verify that you bounds are corrected ordered"
-  assert y_min <= y_max, "Verify that you bounds are corrected ordered"
+    # Compute the sensitivity bounds for the product of the variables
+    a_min = min(x_min * y_min, x_min * y_max, x_max * y_min, x_max * y_max)
+    a_max = max(x_min * y_min, x_min * y_max, x_max * y_min, x_max * y_max)
 
-  # Crop the data using the bounds provided
-  x = np.min(x_max, np.max(x_min, x))
-  y = np.min(y_max, np.max(y_min, y))
+    # Apply the NoisyAverage with Normalization algorithm
+    # from https://books.google.fr/books?id=WFttDQAAQBAJ&pg=PA29
+    normalized_sum_xy = (
+        sum(x * y) - N * (a_min + a_max) / 2 + lap((a_max - a_min) / epsilon)
+    )
+    normalized_sum_x = sum(x) - N * (x_min + x_max) / 2 + lap((x_max - x_min) / epsilon)
+    normalized_sum_y = sum(y) - N * (y_min + y_max) / 2 + lap((y_max - y_min) / epsilon)
 
-  # Compute the DP count once
-  N = len(x) + np.random.laplace(0, 2 / epsilon)
+    if N <= 1:
+        E_xy = (a_min + a_max) / 2
+        E_x = (x_min + x_max) / 2
+        E_y = (y_min + y_max) / 2
+    else:
+        E_xy = normalized_sum_xy / N + (a_min + a_max) / 2
+        E_x = normalized_sum_x / N + (x_min + x_max) / 2
+        E_y = normalized_sum_y / N + (y_min + y_max) / 2
 
-  # Compute the sensitivity bounds for the product of the variables
-  a_min = min(x_min * y_min, x_min * y_max, x_max * y_min, x_max * y_max)
-  a_max = max(x_min * y_min, x_min * y_max, x_max * y_min, x_max * y_max)
-
-  # Apply the NoisyAverage with Normalization algorithm
-  # from https://books.google.fr/books?id=WFttDQAAQBAJ&pg=PA29
-  normalized_sum_xy = sum(x * y) - N * (a_min + a_max) / 2 + lap(0, (a_max - a_min) / epsilon)
-  normalized_sum_x = sum(x) - N * (x_min + x_max) / 2 + lap((x_max - x_min) / epsilon)
-  normalized_sum_y =  sum(y) - N * (y_min + y_max) / 2 + lap(0, (y_max - y_min) / epsilon)
-
-  if N <= 1:
-    E_xy = (a_min + a_max) / 2
-    E_x = (x_min + x_max) / 2
-    E_y = (y_min + y_max) / 2
-  else:
-    E_xy = normalized_sum_xy / N + (a_min + a_max) / 2
-    E_x = normalized_sum_x / N + (x_min + x_max) / 2
-    E_y = normalized_sum_y / N + (y_min + y_max) / 2
-
-  # N - 1 is used to have an unbiased estimate (like standard implem in numpy)
-  return N / (N - 1) * (E_xy - E_x * E_y)
+    # N - 1 is used to have an unbiased estimate (like standard implem in numpy)
+    return N / (N - 1) * (E_xy - E_x * E_y)
--- inst/python/pydp_wrapper/sum_PyDP.py
+++ inst/python/pydp_wrapper/sum_PyDP.py
@@ -3,5 +3,7 @@ from pydp.algorithms.laplacian import BoundedSum
 
 
 def pyDP_bounded_sum(input_data, epsilon, lower_bound, upper_bound):
-  x = BoundedSum(epsilon=epsilon, lower_bound=lower_bound, upper_bound=upper_bound, dtype="float")
-  return x.quick_result(input_data)
+    x = BoundedSum(
+        epsilon=epsilon, lower_bound=lower_bound, upper_bound=upper_bound, dtype="float"
+    )
+    return x.quick_result(input_data)
