d3fe25903fbe881fdd9ae239976ee56be07c214b
Théo Ryffel <theo.leffyr@gmail.com>
2022-06-17 14:19:39 +0200

    style: apply black

--- inst/python/pydp_wrapper/max_PyDP.py
+++ inst/python/pydp_wrapper/max_PyDP.py
@@ -3,5 +3,7 @@ from pydp.algorithms.laplacian import Max
 
 
 def pyDP_max(input_data, epsilon, lower_bound, upper_bound):
-  x = Max(epsilon=epsilon, lower_bound=lower_bound, upper_bound=upper_bound, dtype="float")
-  return x.quick_result(input_data)
+    x = Max(
+        epsilon=epsilon, lower_bound=lower_bound, upper_bound=upper_bound, dtype="float"
+    )
+    return x.quick_result(input_data)
--- inst/python/pydp_wrapper/variance_PyDP.py
+++ inst/python/pydp_wrapper/variance_PyDP.py
@@ -3,5 +3,7 @@ from pydp.algorithms.laplacian import BoundedVariance
 
 
 def pyDP_bounded_variance(input_data, epsilon, lower_bound, upper_bound):
-  x = BoundedVariance(epsilon=epsilon, lower_bound=lower_bound, upper_bound=upper_bound, dtype="float")
-  return x.quick_result(input_data)
+    x = BoundedVariance(
+        epsilon=epsilon, lower_bound=lower_bound, upper_bound=upper_bound, dtype="float"
+    )
+    return x.quick_result(input_data)
