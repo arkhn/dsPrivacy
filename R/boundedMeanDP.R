70499e020961874d547120e4ce2308b30ab0aee0
dokteur <jason.paumier@gmail.com>
2022-07-13 16:08:09 +0200

    fix: use differentially private count in mean result

--- R/boundedMeanDP.R
+++ R/boundedMeanDP.R
@@ -6,13 +6,13 @@
 #' @param lower_bound lower bound for input values
 #' @param upper_bound upper bound for input values
 #'
-#' @return a numeric, differentially private mean of the input vector
+#' @return a named list with 2 differentially private numeric values: the mean (Mean) and the length (Ntotal) of the input vector
 #' @export
 
 
 boundedMeanDP <- function(input_data, epsilon, lower_bound, upper_bound){
-  mean <- py_module$mean_PyDP$pyDP_bounded_mean(input_data, epsilon, lower_bound, upper_bound)
-  total <- length(input_data)
+  mean <- py_module$mean_PyDP$pyDP_bounded_mean(input_data, epsilon / 2, lower_bound, upper_bound)
+  length <- py_module$count_PyDP$pyDP_count(input_data, epsilon / 2)
 
-  return(list(Mean=mean, Ntotal=total))
+  return(list(Mean=mean, Ntotal=length))
 }
