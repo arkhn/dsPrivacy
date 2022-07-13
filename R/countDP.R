1d41a436760d2b7c85e808fb4abf8bdb51537214
dokteur <jason.paumier@gmail.com>
2022-07-13 16:07:08 +0200

    feat: add count with DP

---
+++ R/countDP.R
@@ -0,0 +1,14 @@
+#' @title Differentially private count
+#' @description Computes a differentially private count of the number of elements in a vector
+#'
+#' @param input_data the input vector
+#' @param epsilon privacy budget
+#'
+#' @return a numeric, differentially private count of the number of elements in a vector
+#' @export
+
+
+countDP <- function(input_data, epsilon){
+  res <- py_module$count_PyDP$pyDP_count(input_data, epsilon)
+  return(res)
+}
