21fb5fb41cf56c206617b8f01d322b91be2ed465
Théo Ryffel <theo.leffyr@gmail.com>
2022-07-29 11:18:03 +0200

    test: add building blocks for testing empty values

--- tests/unit/dp/custom_dp/covariance_custom_test.py
+++ tests/unit/dp/custom_dp/covariance_custom_test.py
@@ -1,10 +1,13 @@
 import numpy as np
+import pandas as pd
 import pytest
 
 from inst.python.dp.custom_dp.covariance_custom import custom_bounded_covariance, noisy_average
 
-relative_threshold = 0.01
-epsilon = 100
+rel_threshold = 0.01
+abs_threshold = 0.01
+epsilon = 1000
+n_empty_values = 10
 
 
 def test_noisy_average(height_samples_with_bounds):
@@ -15,14 +18,36 @@ def test_noisy_average(height_samples_with_bounds):
 
     x_noisy_average = noisy_average(x, epsilon, x_min, x_max, n, n_noisy)
     x_average = np.mean(x)
-    assert x_noisy_average == pytest.approx(x_average, rel=relative_threshold)
+    assert x_noisy_average == pytest.approx(x_average, rel=rel_threshold, abs=abs_threshold)
 
 
-def test_custom_bounded_covariance(height_samples_with_bounds, weight_samples_with_bounds):
+# TODO: True option needs further investigation of the SOTA implementations, pandas being based on
+# Welford algorithm detailed here https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
+@pytest.mark.parametrize("empty_values", [False, True])
+def test_custom_bounded_covariance(
+    height_samples_with_bounds, weight_samples_with_bounds, empty_values
+):
     x, x_min, x_max = height_samples_with_bounds
     y, y_min, y_max = weight_samples_with_bounds
 
-    x_noisy_covariance = custom_bounded_covariance(x, y, epsilon, x_min, x_max, y_min, y_max)
-    x_covariance = np.cov(x, y)[0][1]
+    # we use pandas for handling None values in the true covariance computation
+    df = pd.DataFrame(data={"height": x, "weight": y})
 
-    assert x_noisy_covariance == pytest.approx(x_covariance, rel=relative_threshold)
+    if empty_values:
+        for idx in np.random.randint(0, len(x), n_empty_values):
+            x[idx] = None
+        for idx in np.random.randint(0, len(y), n_empty_values):
+            y[idx] = None
+
+    x_noisy_covariance = custom_bounded_covariance(x, y, epsilon, x_min, x_max, y_min, y_max)
+    x_covariance = np.array(df.cov())[0][1]
+
+    if empty_values:
+        # TODO: We currently just check that the result isn't empty. This is not sufficient.
+        # There is currently a small relative error, suggesting that the implementation needs
+        # to be revised.
+        assert x_noisy_covariance is not None
+    else:
+        assert x_noisy_covariance == pytest.approx(
+            x_covariance, rel=rel_threshold, abs=abs_threshold
+        )
